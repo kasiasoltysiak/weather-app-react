e1eb2d0d67154da63e44a5f06a2f32cab6e7cda7
kasiasoltysiak <kat.soltysiak@gmail.com>
2023-08-15 14:30:43 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import './App.css';
 
 
+
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
  
  <h1>hello</h1>
+ 
  <footer>this project was created by Kat Soltysiak and is available open-source on <a href='https://github.com/kasiasoltysiak/weather-app-react' rel='noreferrer' target='_blank'>GitHub</a></footer>
+    </div>
     </div>
   );
 }

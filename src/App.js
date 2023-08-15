9f33a230c8595bfd0d56b5c36939aa67440a72b9
kasiasoltysiak <kat.soltysiak@gmail.com>
2023-08-15 14:06:31 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+ <h1>hello</h1>
     </div>
   );
 }
-

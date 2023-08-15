ec3bdb74ab2a6b22d9871adb2e7ac8734a63e74a
kasiasoltysiak <kat.soltysiak@gmail.com>
2023-08-15 13:59:49 +0200

    undo all the changes to check deployment to netlify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import Weather from './Weather';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
-      <footer>
-        <a href="https://github.com/kasiasoltysiak/weather-app-react" rel="noreferrer" target='blank'>open-source</a></footer>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
+export default App;

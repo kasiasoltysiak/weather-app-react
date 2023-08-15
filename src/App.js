b18b94b35891475bfeb08c92ed80e6747ecaaf14
kasiasoltysiak <kat.soltysiak@gmail.com>
2023-08-15 14:20:41 +0200

    added link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
     <div className="App">
  
  <h1>hello</h1>
+ <footer>this project was created by Kat Soltysiak and is available open-source on <a href='https://github.com/kasiasoltysiak/weather-app-react' rel='noreferrer' target='_blank'>GitHub</a></footer>
     </div>
   );
 }

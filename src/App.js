d13351d36276ac7752462818acff0df053adc41e
kasiasoltysiak <kat.soltysiak@gmail.com>
2023-08-16 18:51:20 +0200

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     <div className="App">
       <div className="container">
      
-     <Weather defaultCity="New York" />
+     <Weather/>
 
      <footer>
        This project was coded by{" "}

621bf33bb7c9f15e0819f8cf3d538c46c6661ae7
kasiasoltysiak <kat.soltysiak@gmail.com>
2023-08-17 21:22:05 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+      
 
         <footer>
           This project was coded by{" "}
